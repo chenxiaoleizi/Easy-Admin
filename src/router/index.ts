10b16626cc40c47b591e8ceea53b8ebefd421c57
chenlei <791563973@qq.com>
2024-09-21 21:50:43 +0800

    feat: ts配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,4 @@
-import {
-  type RouteRecordRaw,
-  createRouter,
-  createWebHashHistory,
-} from "vue-router";
+import { type RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 import staticRoutes from "./static";
 import dynamicRoutes from "./dynamic";
 import { useUserStore } from "@/store/user";
@@ -10,38 +6,39 @@ import { usePermissionStore } from "@/store/permission";
 import { message } from "ant-design-vue";
 
 const router = createRouter({
-  routes: [...staticRoutes, ...dynamicRoutes],
+  routes: [...dynamicRoutes],
   history: createWebHashHistory(),
 });
 
 router.beforeEach(async (to) => {
-  const userStore = useUserStore();
-  const permissionStore = usePermissionStore();
+  return true;
+  // const userStore = useUserStore();
+  // const permissionStore = usePermissionStore();
 
-  if (userStore.token) {
-    // 初始化权限
-    if (!permissionStore.initialized) {
-      try {
-        await permissionStore.initPermission(router);
-      } catch {
-        message.error("加载权限信息出错");
-        userStore.token = "";
-        return "/login";
-      }
-    }
+  // if (userStore.token) {
+  //   // 初始化权限
+  //   if (!permissionStore.initialized) {
+  //     try {
+  //       await permissionStore.initPermission(router);
+  //     } catch {
+  //       message.error("加载权限信息出错");
+  //       userStore.token = "";
+  //       return "/login";
+  //     }
+  //   }
 
-    // 已登录状态，自动跳转到登陆后的页面
-    if (to.path === "/login") {
-      const firstMenu = permissionStore.firstMenu;
-      return firstMenu.path;
-    }
+  //   // 已登录状态，自动跳转到登陆后的页面
+  //   if (to.path === "/login") {
+  //     const firstMenu = permissionStore.firstMenu;
+  //     return firstMenu.path;
+  //   }
 
-    return true;
-  } else {
-    // 未登录
-    if (to.path !== "/login") return "/login";
-    return true;
-  }
+  //   return true;
+  // } else {
+  //   // 未登录
+  //   if (to.path !== "/login") return "/login";
+  //   return true;
+  // }
 });
 
 export default router;
