3ed40cb7d368254b56331e6e9b164d0ae4f00b27
chenlei <791563973@qq.com>
2024-11-17 18:15:04 +0800

    feat: 字典组件

---
+++ src/components/dict/DictSelect.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-select :options="options" v-model:value="value" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
+import { useDictStore } from "@/store/dict";
+const { dictMap } = storeToRefs(useDictStore());
+
+const props = defineProps<{ dictCode: string }>();
+const value = defineModel("value");
+const options = computed(() => {
+  const dict = dictMap.value[props.dictCode] ?? { options: [] };
+  return dict.options ?? [];
+});
+</script>
---
+++ src/components/dict/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>{{ dictItemName }}</span>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
+import { useDictStore } from "@/store/dict";
+const { dictMap } = storeToRefs(useDictStore());
+
+const props = defineProps<{ dictCode: string; dictItemKey: string }>();
+const dictItemName = computed(() => {
+  const dict = dictMap.value[props.dictCode] ?? { map: {} };
+  const map = dict.map;
+  return map[props.dictItemKey] ?? "";
+});
+</script>
